4cab79eb4f5c50b8587bd9adc0a2bb2263aedeaf
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 09:00:52 +0530

    A list object created of type integer

--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class CustomerService {
     // This method returns the customer's loyalty points in sorted order
 public List<Integer> getListOfCustomersSortedByLoyaltyPoints(List<Customer> customerList, LoyaltyPointComparator loyaltyPointComparator){
-
+List<Integer> integerList = new ArrayList<>();
     return null;
 }
 
