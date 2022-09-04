0a7e275c6e22dfd13c16947af941085c028e09c3
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 09:18:01 +0530

    Test case passed

--- src/main/java/com/jap/customers/CustomerService.java
+++ src/main/java/com/jap/customers/CustomerService.java
@@ -7,7 +7,7 @@ public class CustomerService {
     // This method returns the customer's loyalty points in sorted order
 public List<Integer> getListOfCustomersSortedByLoyaltyPoints(List<Customer> customerList, LoyaltyPointComparator loyaltyPointComparator){
 List<Integer> integerList = new ArrayList<>();
-    Collections.sort(customerList,new LoyaltyPointComparator());
+    Collections.sort(customerList,new LoyaltyPointComparator().reversed());
     for(Customer cus : customerList){
         integerList.add(cus.getLoyaltyPoints());
     }
